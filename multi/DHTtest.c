f34a9f8d002ab580f8487560d73c826b4229ca5e
Matthew Haas <haas@corning-cc.edu>
2025-10-02 15:27:39 -0400

    DHTtest.c polishing

--- multi/DHTtest.c
+++ multi/DHTtest.c
@@ -1,7 +1,8 @@
-/*
- *  dht.c:
- *    read temperature and humidity from DHT11 or DHT22 sensor
- */
+///
+/// DHTtest.c: test program that uses libDHT to read from two temperature sensors
+///
+////////////////////////////////////////////////////////////////////////////////////////
+
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +12,18 @@
 int main (void)
 {
     DHT *DHT11                     = NULL;
+    DHT *DHT22                     = NULL;
 
-    DHT_init (&DHT11, 12, SENSOR_TYPE_DHT11);
-
-    fprintf (stdout, "Raspberry Pi DHT11/DHT22 temperature/humidity sensor test\n");
+    fprintf (stdout, "wiringPi / libDHT DHT11/DHT22 temperature/humidity sensor test\n");
 
     if (wiringPiSetup ()          == -1)
     {
         fprintf (stderr, "[ERROR] Could not initialize wiringPi\n");
         exit (1);
     }
+
+    DHT_init (&DHT11, 12, SENSOR_TYPE_DHT11);
+    DHT_init (&DHT22, 13, SENSOR_TYPE_DHT22);
 
     while (1)
     {
@@ -31,9 +34,20 @@ int main (void)
             fprintf (stdout, "[cached] ");
         }
 
-        fprintf (stdout, "Humidity = %.1f %% ",    DHT11 -> humidity);
-        fprintf (stdout, "Temperature = %.1f *C ", DHT11 -> celcius);
-        fprintf (stdout, "(%.1f *F)\n",            DHT11 -> fahrenheit);
+        fprintf (stdout, "DHT11: Humidity = %.1f %% ",    DHT11 -> humidity);
+        fprintf (stdout, "Temperature = %.1f *C ",        DHT11 -> celcius);
+        fprintf (stdout, "(%.1f *F)\n",                   DHT11 -> fahrenheit);
+
+        read_DHT_data (&DHT22);
+
+        if (DHT22 -> cached       == TRUE)
+        {
+            fprintf (stdout, "[cached] ");
+        }
+
+        fprintf (stdout, "DHT22: Humidity = %.1f %% ",    DHT22 -> humidity);
+        fprintf (stdout, "Temperature = %.1f *C ",        DHT22 -> celcius);
+        fprintf (stdout, "(%.1f *F)\n",                   DHT22 -> fahrenheit);
 
         delay (2000); /* wait 2 seconds before next read */
     }
